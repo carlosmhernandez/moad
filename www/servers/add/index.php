cf8bc06d3a58c9921b1bb33d49826f155644db1a
carlosmhernandez <78705654+carlosmhernandez@users.noreply.github.com>
2021-02-18 20:43:17 -0600

    updating format and input fields

--- www/servers/add/index.php
+++ www/servers/add/index.php
@@ -50,60 +50,71 @@
 					</ul>
 				</div>				
 				<p>
-					<form action="submit.php">
-						<label for="hostname">Host Name:</label><input type="text" name="hostname" id="hostname" placeholder="Enter computer name">
-						<label for="fqdn">FQDN:</label><input type="text" name="fqdn" id="fqdn" placeholder="Enter fully qualified domain name">
-						<label for="ipv4address">IPV4 Address:</label><input type="text" name="ipv4address" id="ipv4address">
-						<label for="nodetype">Node Type:</label>
-						<select name="nodetype" id="nodetype">
-							<option value="VM" selected>Virtual</option>
-							<option value="PHYSICAL">Physical</option>
-						</select>
-  					<label for="description">Description:</label><input type="text" name="description" id="description" placeholder="i.e. Home Drives">
-						<label for="type">Type:</label><input type="text" name="type" id="type" placeholder="i.e. File Server">
-						<label for="environment">Environment:</label>
-						<select name="environment" id="environment">
-							<option value="Production" selected>Production</option>
-							<option value="Integration">Integration</option>
-							<option value="Development">Development</option>
-							<option value="Testing">Testing</option>
-							<option value="Experimental">Experimental</option>
-						</select>
-						<label for="disposition">Disposition:</label>
-						<select name="disposition" id="disposition">
-							<option value="Building" selected>Staging</option>
-							<option value="Deployed">Deployed</option>
-							<option value="Discontinue">Decom in Progress</option>
-							<option value="Retired">Retired</option>
-						</select>
-						<label for="manufacturer">Manufacturer:</label>
-						<select name="manufacturer" id="manufacturer">
-						  <option value="VMWare" selected>VM - (VMWare)</option>
-							<option value="HyperV">VM - HyperV</option>
-							<option value="HP" >HP</option>
-							<option value="Dell">Dell</option>
-						</select>
-						<label for="model">Model:</label><input type="text" name="model" id="model" placeholder="Enter computer model or Leave blank for VMs">
-						<label for="sn">Serial Number:</label><input type="text" name="sn" id="sn"  placeholder="Enter computer serial number or Leave blank for VMs">
-						<label for="ilo">iLO:</label><input type="text" name="ilo" id="ilo" placeholder="dns name or ip address for remote management">
-						<label for="osvendor">OS Vendor:</label>
-						<select name="osvendor" id="osvendor">
-							<option value="Microsoft" selected>Microsoft</option>
-							<option value="VMWare">VMWare</option>
-							<option value="Ubuntu">Ubuntu</option>
-							<option value="RedHat">RedHat</option>
-							<option value="SUSE">SUSE</option>
-							<option value="Linux">Linux (Other)</option>
-						</select>
-						<label for="os">OS:</label><input type="text" name="os" id="os">
-						<label for="osversion">OS Version:</label><input type="text" name="osversion" id="osversion">
-						<label for="ntdomain">Domain:</label><input type="text" name="ntdomain" id="ntdomain" placeholder="Enter the AD domain name for this system">
-						<label for="locid">Location:</label><input type="text" name="locid" id="locid">
-						<label for="datacenterid">Data Center:</label><input type="text" name="datacenterid" id="datacenterid">
-						<label for="appid">Application:</label><input type="text" name="appid" id="appid">
+					
+					<form action="submit.php" method="post">
+						<div class="form-group">
+							<label for="hostname">Host Name:</label><input type="text" name="hostname" id="hostname" placeholder="Enter computer name">
+							<label for="fqdn">FQDN:</label><input type="text" name="fqdn" id="fqdn" placeholder="Enter fully qualified domain name">
+							<label for="ipv4address">IPV4 Address:</label><input type="text" name="ipv4address" id="ipv4address">
+							<label for="nodetype">Node Type:</label>
+							<select name="nodetype" id="nodetype">
+								<option value="VM" selected>Virtual</option>
+								<option value="PHYSICAL">Physical</option>
+							</select>
+							<label for="description">Description:</label><input type="text" name="description" id="description" placeholder="i.e. Home Drives">
+							<label for="type">Type:</label><input type="text" name="type" id="type" placeholder="i.e. File Server">
+							<label for="environment">Environment:</label>
+							<select name="environment" id="environment">
+								<option value="Production" selected>Production</option>
+								<option value="Integration">Integration</option>
+								<option value="Development">Development</option>
+								<option value="Testing">Testing</option>
+								<option value="Experimental">Experimental</option>
+							</select>
+							<label for="disposition">Disposition:</label>
+							<select name="disposition" id="disposition">
+								<option value="Building" selected>Staging</option>
+								<option value="Deployed">Deployed</option>
+								<option value="Discontinue">Decom in Progress</option>
+								<option value="Retired">Retired</option>
+							</select>
+							<label for="manufacturer">Manufacturer:</label>
+							<select name="manufacturer" id="manufacturer">
+								<option value="VMWare" selected>VM - (VMWare)</option>
+								<option value="HyperV">VM - HyperV</option>
+								<option value="HP" >HP</option>
+								<option value="Dell">Dell</option>
+							</select>
+							<label for="model">Model:</label><input type="text" name="model" id="model" placeholder="Enter computer model or Leave blank for VMs">
+							<label for="sn">Serial Number:</label><input type="text" name="sn" id="sn"  placeholder="Enter computer serial number or Leave blank for VMs">
+							<label for="ilo">iLO:</label><input type="text" name="ilo" id="ilo" placeholder="dns name or ip address for remote management">
+							<label for="platform">Platform:</label>
+							<select name="platform" id="platform">
+								<option value="Windows" selected>Windows</option>
+								<option value="Linux">Linux</option>
+								<option value="ESX" >ESX</option>
+								<option value="Appliance">Appliance</option>
+							</select>
+							<label for="osvendor">OS Vendor:</label>
+							<select name="osvendor" id="osvendor">
+								<option value="Microsoft" selected>Microsoft</option>
+								<option value="VMWare">VMWare</option>
+								<option value="Ubuntu">Ubuntu</option>
+								<option value="RedHat">RedHat</option>
+								<option value="SUSE">SUSE</option>
+								<option value="Linux">Linux (Other)</option>
+							</select>
+							<label for="os">OS:</label><input type="text" name="os" id="os">
+							<label for="osversion">OS Version:</label><input type="text" name="osversion" id="osversion">
+							<label for="ntdomain">Domain:</label><input type="text" name="ntdomain" id="ntdomain" placeholder="Enter the AD domain name for this system">
+							<label for="locid">Location:</label><input type="text" name="locid" id="locid">
+							<label for="datacenterid">Data Center:</label><input type="text" name="datacenterid" id="datacenterid">
+							<label for="appid">Application:</label><input type="text" name="appid" id="appid">
 
-						<button>Submint</button>
+							<button>Submint</button>
+						</div>
 					</form>
+				
 				</p>
 		
 			</article>

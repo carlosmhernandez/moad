e46b72c418014fdd83ed1996994c8eab4f257b66
carlosmhernandez <78705654+carlosmhernandez@users.noreply.github.com>
2021-02-19 20:28:13 -0600

    Updating ad database schema

--- provision/mysql/init/01-databases.sql
+++ provision/mysql/init/01-databases.sql
@@ -272,7 +272,7 @@ COMMIT;
 -- https://www.phpmyadmin.net/
 --
 -- Host: database:3306
--- Generation Time: Feb 16, 2021 at 01:41 AM
+-- Generation Time: Feb 20, 2021 at 02:25 AM
 -- Server version: 8.0.23
 -- PHP Version: 7.4.15
 
@@ -295,6 +295,7 @@ USE `ad`;
 DROP TABLE IF EXISTS `computers`;
 CREATE TABLE `computers` (
   `dn` varchar(250) NOT NULL,
+  `ou` varchar(250) NOT NULL,
   `cn` varchar(200) NOT NULL,
   `Description` varchar(200) NOT NULL,
   `domain` varchar(100) NOT NULL,
@@ -311,20 +312,56 @@ CREATE TABLE `computers` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `groupmemberships`
+--
+
+DROP TABLE IF EXISTS `groupmemberships`;
+CREATE TABLE `groupmemberships` (
+  `groupdn` varchar(250) NOT NULL,
+  `usercn` varchar(250) DEFAULT NULL,
+  `userdn` varchar(250) NOT NULL,
+  `domain` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+CREATE TABLE `groups` (
+  `dn` varchar(250) NOT NULL,
+  `ou` varchar(250) NOT NULL,
+  `cn` varchar(200) NOT NULL,
+  `Description` tinytext,
+  `TotalMembers` int DEFAULT NULL,
+  `Domain` varchar(100) DEFAULT NULL,
+  `Type` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `dn` varchar(250) NOT NULL,
+  `ou` varchar(250) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `cn` varchar(200) NOT NULL,
-  `Enabled` varchar(10) NOT NULL,
-  `GivenName` varchar(100) NOT NULL,
-  `Surname` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `DisplayName` varchar(200) NOT NULL,
-  `SID` varchar(50) NOT NULL,
-  `Domain` varchar(100) NOT NULL,
-  `UserPrincipalName` varchar(200) NOT NULL
+  `Enabled` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `GivenName` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `Surname` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `DisplayName` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `email` varchar(250) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `SID` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `Domain` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `UserPrincipalName` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `UAC` int DEFAULT NULL,
+  `GroupMemberships` int DEFAULT NULL,
+  `WhenCreated` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -337,5 +374,49 @@ CREATE TABLE `users` (
 ALTER TABLE `computers`
   ADD PRIMARY KEY (`dn`),
   ADD KEY `cn` (`cn`),
+  ADD KEY `domain` (`domain`),
+  ADD KEY `ou` (`ou`),
+  ADD KEY `WhenChanged` (`WhenChanged`),
+  ADD KEY `Enabled` (`Enabled`),
+  ADD KEY `OperatingSystem` (`OperatingSystem`),
+  ADD KEY `sid` (`sid`),
+  ADD KEY `SamAccountName` (`SamAccountName`),
+  ADD KEY `IPV4Address` (`IPV4Address`),
+  ADD KEY `WhenCreated` (`WhenCreated`),
+  ADD KEY `PasswordLastSet` (`PasswordLastSet`);
+
+--
+-- Indexes for table `groupmemberships`
+--
+ALTER TABLE `groupmemberships`
+  ADD PRIMARY KEY (`groupdn`,`userdn`),
+  ADD KEY `usercn` (`usercn`),
   ADD KEY `domain` (`domain`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`dn`),
+  ADD KEY `ou` (`ou`),
+  ADD KEY `cn` (`cn`),
+  ADD KEY `Type` (`Type`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`dn`),
+  ADD KEY `ou` (`ou`),
+  ADD KEY `cn` (`cn`),
+  ADD KEY `Enabled` (`Enabled`),
+  ADD KEY `GivenName` (`GivenName`),
+  ADD KEY `Surname` (`Surname`),
+  ADD KEY `DisplayName` (`DisplayName`),
+  ADD KEY `UAC` (`UAC`),
+  ADD KEY `Domain` (`Domain`),
+  ADD KEY `SID` (`SID`),
+  ADD KEY `email` (`email`),
+  ADD KEY `GroupMemberships` (`GroupMemberships`),
+  ADD KEY `WhenCreated` (`WhenCreated`);
 COMMIT;

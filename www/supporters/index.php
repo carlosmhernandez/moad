0111dbff0f06c7ea7114903181fb4f6de3841c97
carlosmhernandez <78705654+carlosmhernandez@users.noreply.github.com>
2021-02-20 19:48:39 -0600

    Create index.php

---
+++ www/supporters/index.php
@@ -0,0 +1,69 @@
+<?PHP
+  if (! defined('ROOT_PATH')) {
+    define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR);
+  }
+
+  require_once ROOT_PATH . 'includes/common.inc.php';
+  
+  echo "<!DOCTYPE html>";
+  echo "<html lang=\"en\">";
+  echo "<head>";
+
+  include ROOT_PATH . 'includes/meta.inc.php';
+
+  // PAGE TITLE HERE
+  echo "<title>MOAD - Supporters</title>";
+
+	// STYLESHEETS
+  echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/style.css\">";
+  echo "</head>";
+
+	echo "<body>";
+  
+	// Content 
+?>
+  <div id="content">
+		<div class="inner">
+
+	    <!-- Post -->
+			<article class="box post post-excerpt">
+				<header>
+					<h2>Suporters</h2>
+					<p>Technical Support Teams</p>
+				</header>
+				<div class="info">
+<?php
+				$year = date("Y");
+				$month = date("M");
+				$day   = date("d");
+				
+				echo "<span class=\"date\"><span class=\"month\">", $month;
+				echo "</span> <span class=\"day\">";
+				echo $day, "</span><span class=\"year\">, ";
+				echo $year, "</span></span>";
+
+?>
+					<ul class="stats">
+						<li><a href="#" class="fas fa-bell">16</a></li>
+						<li><a href="#" class="fas fa-server">32</a></li>
+						<li><a href="#" class="fas fa-exclamation-triangle">64</a></li>
+					</ul>
+				</div>				
+				<p>
+					<ul>
+						<li><a href="/supporters/add">Add Support Team Member</a></li>
+					</ul>
+
+				</p>
+		
+			</article>
+		</div>
+	</div>
+
+<?php
+  $menu_selection = "supporters";
+	include  ROOT_PATH . 'includes/sidebar.inc.php';
+	
+echo "</body>";
+echo "</html>";
+?>

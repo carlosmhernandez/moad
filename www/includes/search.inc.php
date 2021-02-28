4f25bdd455baddf299235684b760af7e6f60e4eb
carlosmhernandez <78705654+carlosmhernandez@users.noreply.github.com>
2021-02-27 20:10:13 -0600

    correcting select tag

--- www/includes/search.inc.php
+++ www/includes/search.inc.php
@@ -1,15 +1,9 @@
 <?php
     // Search 
-/*    echo "<section class=\"box search\">";
-      echo "<form method=\"post\" action=\"#\">";
-        echo "<input type=\"text\" class=\"text\" name=\"search\" placeholder=\"Search\" />";
-      echo "</form>";
-    echo "</section>";
-*/
     echo "<form method=\"get\" name=\"search\" id=\"searchform\" action=\"/finder/\">";
     echo "<input name=objtype id=objtype type=hidden>";
     echo "<span style=\"padding:5em;width:100%;text-shadow:none\">";
-    echo "<select class=\"query\" name=\"state\" id=\"query\" onChange=\"this.form.submit()\">";
+    echo "<select class=\"query\" name=\"query\" id=\"query\" onChange=\"this.form.submit()\">";
     echo "</select>";
     echo "</span>";
     echo "</form>";

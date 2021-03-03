7d085d01c47a3d6e6e6e857865061ee651da66a7
carlosmhernandez <78705654+carlosmhernandez@users.noreply.github.com>
2021-03-02 19:48:26 -0600

    Adding link to Users

--- www/includes/sidebar.inc.php
+++ www/includes/sidebar.inc.php
@@ -10,6 +10,7 @@
       echo "<ul>";
         echo "<li "; if ($menu_selection == 'home') echo "class=\"current\""; echo "><a href=\"/\">Home</a></li>";
         echo "<li "; if ($menu_selection == 'servers') echo "class=\"current\""; echo "><a href=\"/servers/\">Servers</a></li>";
+        echo "<li "; if ($menu_selection == 'users') echo "class=\"current\""; echo "><a href=\"/users/\">Users</a></li>";
         echo "<li "; if ($menu_selection == 'locations') echo "class=\"current\""; echo "><a href=\"/locations/\">Locations</a></li>";
         echo "<li "; if ($menu_selection == 'applications') echo "class=\"current\""; echo "><a href=\"/applications/\">Applications</a></li>";
         echo "<li "; if ($menu_selection == 'supporters') echo "class=\"current\""; echo "><a href=\"/supporters/\">Supporters</a></li>";
